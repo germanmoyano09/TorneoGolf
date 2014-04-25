c1ea50e6203b85322a753b3170de171e0c878ad1
German Moyano <c.germanmoyano@gmail.com>
2014-04-25 00:59:56 -0300

    modifico index

--- index.php
+++ index.php
@@ -51,16 +51,17 @@
 	
 	<!-- slider -->
 	<div class="myslider">
-		<img src="img/slider/slider1.jpg" alt="" >
+		<img src="img/slider/slider1.jpg" alt="">
 		<img src="img/slider/slider2.jpg" alt="">
 		<img src="img/slider/slider3.jpg" alt="">
+		<img src="img/slider/slider2.jpg" alt="">
 	</div>
 	<!-- /slider -->
 	
 	<!-- banner -->	
 	<div id="banner">
 		<div class="welcome_index">
-			I think most  spend the first 5 years of their career mastering and the rest of their lives learning  - Buzz Andersen
+			Cada vez falta menos para nuestro próximo torneo. Por cualquier consulta o recomendación puede conctactarnos en la sección de contacto.
 		</div>
 		<!-- Countdown-->
 		<div class="mainWrapper">
@@ -74,85 +75,6 @@
 	</div>
 	<!--//banner-->
 		
-		
-		
-	<div class="container">
-		<div class="inner_content">
-			<div class="pad30"></div>
-			<!--info boxes-->
-			<div class="span12">
-				<div class="row">
-					<div class="row">
-						<div class="span3">
-								<div class="tile">
-								 <img class="tile-image big-illustration" alt="" src="img/illustrations/shield.png" />
-								<h3 class="tile-title">Flat Design</h3>
-								<p>Nam ea labitur pericula. Meis tamquam pro te pri id mazim nostrud id mazim nostrud diceret.</p>
-								<h6><a class="btn btn-custom" href="#"><b>read more</b></a></h6>
-							</div>
-						</div>
-					
-						<div class="span3">
-							<div class="tile">
-								<img class="tile-image" alt="" src="img/illustrations/companies.png" />
-								<h3 class="tile-title">Customize</h3>
-								<p>Nam ea labitur pericula. Meis tamquam pro te pri id mazim nostrud id mazim nostrud diceret.</p>
-								<h6><a class="btn btn-custom" href="#"><b>read more</b></a></h6>
-							</div>
-						</div>
-					
-						<div class="span3">
-							<div class="tile">
-								<img class="tile-image" alt="" src="img/illustrations/phone.png" />
-								<h3 class="tile-title">Responsive</h3>
-								<p>Nam ea labitur pericula. Meis tamquam pro te pri id mazim nostrud id mazim nostrud diceret.</p>
-								<h6><a class="btn  btn-custom" href="#"><b>read more</b></a></h6>
-							</div>
-						</div>
-					
-						<div class="span3">
-						  <div class="tile tile-hot">
-							<img class="tile-image big-illustration" alt="" src="img/illustrations/location.png" />
-							<h3 class="tile-title">Modern</h3>
-							<p>Nam ea labitur pericula. Meis tamquam pro te pri id mazim nostrud id mazim nostrud diceret.</p>
-							 <h6><a class="btn  btn-custom" href="#"><b>read more</b></a></h6>
-						  </div>
-						</div>
-					</div>
-				  <!-- /tiles -->
-				</div>
-			</div> 
-			<!--//info boxes-->
-			
-			<div class="row">
-				<!--col 1-->
-				<div class="span12">
-					<div class="row">
-						<div class="pad30 hidden-phone"></div>	
-						
-						<div class="span4">
-							<h1>Recent Work</h1>
-							<h4>Link to a PrettyPhoto enlarged image or to a page, set as squares or round images.</h4>
-							<p>Nam ea labitur pericula. Meis tamquam pro te, cibo mutat necessitatibus id vim. An his tamquam postulant, pri id mazim nostrud 
-							diceret sapientem eloquentiam sea cu.</p>
-							
-							<a href="portfolio_2columns.html" class="btn btn-1 btn-1b">view portfolio</a>
-								
-							<div class="pad45"></div>
-						</div>
-						
-						<!--column 2 circle slider-->
-						<div class="span8 pad25">
-							<!--circle slider-->
-							<div class="content_slider_wrapper" id="slider1"></div>
-							<div class="pad45"></div>		
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-			
 	<!-- footer -->
 		<?php include_once("fijo/footer.php"); ?>
 	<!-- /footer -->	
@@ -161,72 +83,38 @@
 	<a href="#"><i class="go-top hidden-phone hidden-tablet  icon-double-angle-up"></i></a>
 	<!--//end-->
 						
-<!-- scripts -->
+	<!-- scripts -->
 	<script src="js/jquery.js"></script>			
 	<script src="js/bootstrap.min.js"></script>	
 	<script type="text/javascript" src="js/scripts.js"></script>
 	<script src="js/functions.js" type="text/javascript"></script>
 	<script src="js/jquery.content_slider.min.js" type="text/javascript"></script>
-
-<!-- circle slider ADD YOUR IMAGES HERE -->
-<script type="text/javascript">
-//<![CDATA[
-	(function($){
-		$(document).ready(function() {
-			var image_array = new Array();
-			image_array = [
-				{image: 'img/work_slide/1.jpg', link_url: 'img/work_slide/1.jpg', link_rel: 'prettyPhoto'},
-				{image: 'img/work_slide/2.jpg', link_url: 'img/work_slide/2.jpg', link_rel: 'prettyPhoto'},
-				{image: 'img/work_slide/3.jpg', link_url: 'img/work_slide/3.jpg', link_rel: 'prettyPhoto'},
-				{image: 'img/work_slide/4.jpg', link_url: 'img/work_slide/4.jpg', link_rel: 'prettyPhoto'},
-				{image: 'img/work_slide/5.jpg', link_url: 'img/work_slide/5.jpg', link_rel: 'prettyPhoto'},
-				{image: 'img/work_slide/6.jpg', link_url: 'img/work_slide/6.jpg', link_rel: 'prettyPhoto'},
-				{image: 'img/work_slide/7.jpg', link_url: 'img/work_slide/7.jpg', link_rel: 'prettyPhoto'}
-			];
-			$('#slider1').content_slider({		
-			map : image_array,				
-			max_shown_items:5,	// number of visible circles
-			automatic_height_resize:1,
-			wrapper_text_max_height:300,
-			active_item: 0,					
-			border_on_off:0,
-			allow_shadow:0,
-			enable_mousewheel:0
-			});
-			$("a[rel^='prettyPhoto']").prettyPhoto();
-			jQuery("a[rel^='prettyPhoto'], a[rel^='lightbox']").prettyPhoto({
-			overlay_gallery: false, social_tools: false,  deeplinking: false
-			});
-		});
-	})(jQuery);
-	//]]>
-</script>
 	
-<!-- nerve slider -->
-<script src="js/jquery-ui.min.js"></script>
-<script type="text/javascript" src="js/jquery.nerveSlider.min.js"></script>
-<script>
-//<![CDATA[
-	$(document).ready(function() {
-	$(".myslider").show();
-	$(".myslider").startslider({
-		slideTransitionSpeed: 500,
-		slideTransitionEasing: "easeOutExpo",
-		slidesDraggable: true,
-		sliderResizable: true,
-		showDots:true,
+	<!-- nerve slider -->
+	<script src="js/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="js/jquery.nerveSlider.min.js"></script>
+	<script>
+	//<![CDATA[
+		$(document).ready(function() {
+		$(".myslider").show();
+		$(".myslider").startslider({
+			slideTransitionSpeed: 500,
+			slideTransitionEasing: "easeOutExpo",
+			slidesDraggable: true,
+			sliderResizable: true,
+			showDots:true,
+			});
 		});
-	});
 	//]]>
 	</script>
-	
+		
 	<!--    COUNTDOWN -->
-<!-- JS start Here-->
-<script src="CountDown/js/flipclock/libs/prefixfree.min.js"></script>
-<script src="CountDown/js/flipclock/flipclock.min.js"></script>
-​<script src="CountDown/js/custom.js"></script>
-<!-- JS end Here
-<!--    /COUNTDOWN -->
-	
-	</body>
+	<!-- JS start Here-->
+	<script src="CountDown/js/flipclock/libs/prefixfree.min.js"></script>
+	<script src="CountDown/js/flipclock/flipclock.min.js"></script>
+	​<script src="CountDown/js/custom.js"></script>
+	<!-- JS end Here
+	<!--    /COUNTDOWN -->
+		
+</body>
 </html>
