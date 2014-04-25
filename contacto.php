fc1f70ea17c9b2c8dc7db2596afe946a9026bbc3
German Moyano <c.germanmoyano@gmail.com>
2014-04-25 02:51:59 -0300

    modifico contacto

--- contacto.php
+++ contacto.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>iLEVEL| responsive html5 template | themeforest | josweb</title>
+<title>Torneo Petrolero Solidario - Contacto</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="">
 <meta name="author" content="">
@@ -32,38 +32,40 @@
 		<?php include_once("fijo/header.php"); ?>
 	<!--//header-->
 								
-								<div class="container">
-							<div class="inner_content">
-						<h1 class="title">Get In Touch</h1>
-					<h1 class="intro">It's art if it can't be explained. It's <span class="mid">fashion</span> if no one asks for an <span>explanation.</span> 
-					It's <span class="colour">design</span> if it doesn't need explanation. - Wouter Stokkel</h1>
-					</div>
-						</div>
+	<div class="container">
+		<div class="inner_content">
+			<h1 class="title">Get In Touch</h1>
+			<h1 class="intro">It's art if it can't be explained. It's <span class="mid">fashion</span> if no one asks for an <span>explanation.</span> 
+			's <span class="colour">design</span> if it doesn't need explanation. - Wouter Stokkel</h1>
+		</div>
+	</div>
 			
-			<!--//GOOGLE MAP -ADD YOUR EMBED INFO HERE-->
-			<div id="map">
-			<iframe src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Mornington+Crescent,+Camden+High+Street,+London&amp;aq=1&amp;oq=mornington+crescent&amp;sll=53.11546,-3.939972&amp;sspn=0.458287,1.234589&amp;ie=UTF8&amp;hq=Mornington+Crescent,+Camden+High+Street,+London&amp;t=m&amp;ll=51.535021,-0.13919&amp;spn=0.013881,0.032015&amp;output=embed">
-			</iframe>
-			</div>
+	<!--//GOOGLE MAP -ADD YOUR EMBED INFO HERE-->
+	<div id="map">
+	<!--
+	<iframe src="https://mapsengine.google.com/map/embed?mid=zd1V8b2O47FA.kL8KgwO_eHxU" scrolling="no"></iframe>
+	-->
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7597.577273133003!2d-63.21002224075317!3d-17.80162803668827!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93f1c29f5a823f5f%3A0x2dacab18d8054d47!2sCountry+Club+Las+Palmas!5e0!3m2!1ses-419!2s!4v1398402771319" frameborder="0" scrolling="no" style="border:0" ></iframe>	
+	
+	</div>
 			
-			<div class="container">
-			<div class="inner_content">
-		
-				<div class="row">
-					<div class="span4">
-					<h3>Do you need a website, have a question or comment?</h3> <p>Please feel free to send us an email or fill in our handy contact form. We aim to reply within 24 hours.</p>
+	<div class="container">
+		<div class="inner_content">
+			<div class="row">
+				<div class="span4">
+					<h3>Do you need a website, have a question or comment?</h3> 
+					<p>Please feel free to send us an email or fill in our handy contact form. We aim to reply within 24 hours.</p>
 					
-				<p>
-					<b>Contact Information</b><br>
-					iLevel Studios<br>
-					23 Mornington Crescent,<br>
-					Camden High Street,<br>
-					London<br>
-					NW1 7JE
-				</p>
-				
-				
-				<ul class="icons">
+					<p>
+						<b>Contact Information</b><br>
+						iLevel Studios<br>
+						23 Mornington Crescent,<br>
+						Camden High Street,<br>
+						London<br>
+						NW1 7JE
+					</p>
+					
+					<ul class="icons">
 						<li><i class="icon-envelope colour"></i> <a href="mailto:#">name@example.com</a></li>
 						<li><i class="icon-phone colour"></i> +44 020 2345 1987</li>
 						<li><i class="icon-globe colour"></i> <a href="#">www.example.com</a></li>
@@ -71,62 +73,45 @@
 					</ul>
 				</div>
 		
-		<div class="span8 ">
-		<div class="contact_form">  
-		
-		<form id="ajax-contact-form">
-			<p class="form_info">name <span class="required">*</span></p>
-			<input class="span5" type="text" name="name" value="" />
-			<p class="form_info">email <span class="required">*</span></p>
-			<input class="span5" type="text" name="email" value=""  />
-			<p class="form_info">subject</p>
-			<input class="span5" type="text" name="subject" value="" /><br>
-			<p class="form_info">message</p>
-			<textarea name="message" id="message" class="span8" ></textarea>
-			<div class="clear"></div>
-			<input type="submit" class="btn  btn-large btn-primary marg-right5 hue" value="submit" />
-			<input type="reset"  class="btn   btn-large btn-primary" value="reset" />
-			<div class="clear"></div>
-		</form>
-	</div>
-	</div>                	
-		</div>
+				<div class="span8 ">
+					<div class="contact_form">  
+						<form id="ajax-contact-form">
+							<p class="form_info">name <span class="required">*</span></p>
+							<input class="span5" type="text" name="name" value="" />
+							<p class="form_info">email <span class="required">*</span></p>
+							<input class="span5" type="text" name="email" value=""  />
+							<p class="form_info">subject</p>
+							<input class="span5" type="text" name="subject" value="" /><br>
+							<p class="form_info">message</p>
+							<textarea name="message" id="message" class="span8" ></textarea>
+							<div class="clear"></div>
+							<input type="submit" class="btn  btn-large btn-primary marg-right5 hue" value="submit" />
+							<input type="reset"  class="btn   btn-large btn-primary" value="reset" />
+							<div class="clear"></div>
+						</form>
+					</div>
+				</div>                	
 			</div>
-				</div>
-				<!--//page-->
-				<div class="pad45"></div>
+		</div>
+	</div>
+	
+	<!--//page-->
+	<div class="pad45"></div>
 				
-				<!--footer-->
-				<div id="footer">
-		<div class="container">
-			<div class="row">
-				<div class="span12">
-				<div class="copyright">
-							ILEVEL
-							&copy;
-							<script type="text/javascript">
-							//<![CDATA[
-								var d = new Date()
-								document.write(d.getFullYear())
-								//]]>
-								</script>
-							 - All Rights Reserved :
-							Template by <a href="http://spiralpixel.com/">Spiral Pixel</a>
-						</div>
-						</div>
-					</div>
-				</div>
-					</div>
-					<!-- up to top -->
-				<a href="#"><i class="go-top hidden-phone hidden-tablet  icon-double-angle-up"></i></a>
-				<!--//end--> 
-				</div>
+	<!-- footer -->
+		<?php include_once("fijo/footer.php"); ?>
+	<!-- /footer -->
+	
+	<!-- up to top -->
+	<a href="#"><i class="go-top hidden-phone hidden-tablet  icon-double-angle-up"></i></a>
+	
+	<!--//end-->
 				
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
 <script src="js/functions.js" type="text/javascript"></script>
 		
-</script>
+
 </body>
 </html>
