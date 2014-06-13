a4439ddda95f5aac02a2d201617cc16b2dfe7eda
German Moyano <c.germanmoyano@gmail.com>
2014-06-13 17:03:56 -0300

    agrego 404

---
+++ 404.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Torneo Petrolero Solidario</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700,300' rel='stylesheet' type='text/css'>
+<!--[if IE]>
+	<link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Lato:400" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Lato:700" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet" type="text/css">
+<![endif]-->
+
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link href="css/theme.css" rel="stylesheet">
+<link href="css/prettyPhoto.css" rel="stylesheet" type="text/css"/>
+<link href="css/zocial.css" rel="stylesheet" type="text/css"/>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!--[if IE 7]>
+<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
+<![endif]-->
+
+</head>
+<body>
+	<!--header-->
+		<?php $titulo = "index"; ?>
+		<?php include_once("fijo/header.php"); ?>
+	<!--//header-->
+	
+								
+	<!--page-->
+	<div class="container">
+		<div class="inner_content">
+			<h1 class="title">Error 404</h1>
+			<h1 class="intro">La página que <span>estás buscando</span> no existe, por favor 
+			<a href="index.php"><span>continue navegando!</span></a></h1>
+		
+			<div class="row">
+				<div class="span12 screen-bg">
+					<div class="row">
+						<div class="span8 offset2">
+							<img src="img/whoops.png" class=" animated swing" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="pad30"></div>
+<!--//page-->
+<!-- footer -->
+	<div id="footer_index">
+		<?php include_once("fijo/footer.php"); ?>
+	</div>	
+<!-- /footer -->	
+					<!-- up to top -->
+				<a href="#"><i class="go-top hidden-phone hidden-tablet  icon-double-angle-up"></i></a>
+				<!--//end--> 
+			</div>
+			
+<!-- scripts -->
+<script src="js/jquery.js"></script>			
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+<script src="js/functions.js" type="text/javascript"></script>
+    
+</body>
+</html>
