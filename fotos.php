9b2385ba1818d9759ff7e47574fc420acbe1b3a2
German Moyano <c.germanmoyano@gmail.com>
2014-07-10 20:17:00 -0300

    agrego favicon apple

--- fotos.php
+++ fotos.php
@@ -19,6 +19,15 @@
 <link rel="icon" href="img/favicon/favicon.ico" type="image/x-icon" />
 <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon" />
 
+<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png" />
+
 <link href='http://fonts.googleapis.com/css?family=Lato:400,700,300' rel='stylesheet' type='text/css'>
 <!--[if IE]>
 	<link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
